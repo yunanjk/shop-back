cd552ff540b91fafc16604f368966095c4f153b4
oyy <oyy0821@chunjae.co.kr>
2024-11-04 14:11:06 +0900

    초기 세팅

---
+++ src/test/java/com/oyy/shop/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oyy.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
